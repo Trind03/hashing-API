989447c51729fc5ecbb814216fdd8e2b9ae8e916
Torstein <141224356+Trind03@users.noreply.github.com>
2024-09-21 09:44:19 +0200

    Update

--- src/common.py
+++ src/common.py
@@ -2,7 +2,7 @@ import socket
 
 class internet_computer:
     def __init__(self,IP,PORT) -> None:
-        self.AUTH_TOKEN = ""
+        self.AUTH_TOKEN = "5c304a425e04d1fcba6a43853e6b10ffe35c725dc0ccc1bec0be8194264e4f44c17163b57cdadd88573f4f5f51c378af0ea2440081a9f1c0056204e71f2682dbA"
 
         self._Header = 64
         self._IP = IP
