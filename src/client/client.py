737b4b28a56985f9e55274ed33fe4739aeb48228
Torstein <141224356+Trind03@users.noreply.github.com>
2024-09-22 21:11:28 +0200

    Update
    Added functionality to connect to server and added function for startup and maintaining a running workflow.

--- src/client/client.py
+++ src/client/client.py
@@ -1,9 +1,22 @@
+import socket
+from sys import path
+path.append("../")
+from common import internet_computer
 
 
-class client:
+class client(internet_computer):
 
-    def __init__(self) -> None:
-        ...
+    def __init__(self,IP_,PORT_) -> None:
+        super().__init__(IP_,PORT_)
+        self._Sock = socket.socket(self._IP_FAMILY,self._PROTOCOL)
+        self._Running = True
+
+    def Startup(self):
+        self._Sock.connect((self._IP,self._PORT))
+
+    def Running(self):
+        while(self._Running):
+            ...
 
 
     def __del__(self):
--- src/client/main.py
+++ src/client/main.py
@@ -1,7 +1,6 @@
 import socket
-
-
+from client import client
 def main():
-    ...
+    Client: client = client("127.0.0.1",5416)        
 
 main()
