b61e730aa10091495f86dc5feb2b783bd36fd867
Torstein <141224356+Trind03@users.noreply.github.com>
2024-09-22 22:48:30 +0200

    Update
    Added function to process header.

--- src/common.py
+++ src/common.py
@@ -2,6 +2,7 @@ import socket
 
 class internet_computer:
     def __init__(self,IP,PORT) -> None:
+        # Authenticaion 
         self.AUTH_TOKEN = "5c304a425e04d1fcba6a43853e6b10ffe35c725dc0ccc1bec0be8194264e4f44c17163b57cdadd88573f4f5f51c378af0ea2440081a9f1c0056204e71f2682dbA"
 
         self._Header = 64
@@ -9,6 +10,15 @@ class internet_computer:
         self._PORT = PORT
         self._IP_FAMILY = socket.AF_INET
         self._PROTOCOL = socket.SOCK_STREAM
+
+
+    # Protocol will first broadcast a header: size of data. This function pads out the header with ' ' to match expected size.
+    def Process_header(self, Size: int):
+        TARGETS = len(Size) - self._Header
+        DATA = str(self._Header)
+        return DATA.ljust(TARGETS)
+        
+
 
 
     def __del__(self) -> None:
