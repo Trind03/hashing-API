199b37f84dd0336b9fd6f36f75ab9e4c8fe0aee6
Torstein <torsrinde@gmail.com>
2024-09-24 22:05:26 +0200

    Added id to client

--- src/server/client.py
+++ src/server/client.py
@@ -4,11 +4,13 @@ sys.path.append("../")
 import common
 
 class client:
-    def __init__(self,Sock:socket.socket, Addr) -> None:
+    def __init__(self,Sock:socket.socket, Addr,ID:int) -> None:
         print(f"Client -> {Addr} Connected")
+        self._ID: int = ID
         self._Sock = Sock
         self._Address = Addr
     
     def __del__(self):
         print(f"Client: -> {self._Address} Disconnected")
         self._Sock.send(common.flags.Disconnect.encode())
+        self._Sock.close()
