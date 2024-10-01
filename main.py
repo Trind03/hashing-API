05aba78b340267cf9a7a7fcd308cc7fdd142d115
Torstein <torsrinde@gmail.com>
2024-10-01 23:37:10 +0200

    Update and restructuring of code

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
 import pickle
+import uvicorn
 
+# Request format
+# addr:port/algorithm/value
 
-app = FastAPI()
-@app.get("/")
-def broadcast():
-    data = pickle.dumps("Hello world")
-    return {f"{data}"}
+class server:
+    def __init__(self,HOST,PORT) -> None:
+        self._HOST: str = HOST
+        self._PORT: int = PORT
+        self._APP: FastAPI  = FastAPI()
+
+        @self._APP.get("/")
+        def broadcast():
+            return {f"Error-404"}
+    
+    def run(self):
+        uvicorn.run(self._APP,host=self._HOST,port=self._PORT)
+
 
 if(__name__ == "__main__"):
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0",port = 4334)
+    Server: server = server("0.0.0.0",4334)
+    Server.run()
