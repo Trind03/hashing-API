d287d216852ea7626eaaaefcc7591e97bb61f456
Torstein <torsrinde@gmail.com>
2024-10-02 10:32:36 +0200

    Update
    Adding logic for selecting algorithm.

--- server.py
+++ server.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
-
+import hashlib
 # Request format
 # addr:port/algorithm/value
+
+def process_data(data:str,Algorithm:str):
+    Hash: hashlib = hashlib()
+
+    match Algorithm:
+
+        case "MD5"
 
 class server:
     def __init__(self,HOST,PORT) -> None:
@@ -18,6 +25,8 @@ class server:
         def list_algo():
             data = ["MD5","SHA256", "SHA1","SHA512"]
             return data;
-        
+        @self._APP.get("/{algorithm}/{data}")
+        def Process_data_ret():
+            ...
     def Running(self):
         uvicorn.run(self._APP,host=self._HOST,port=self._PORT)
